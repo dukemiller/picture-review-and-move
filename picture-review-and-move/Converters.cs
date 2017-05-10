0860b9e5e7574f2a7c6e4811fd504a0538137345
dukemiller <dukemiller928@hotmail.com>
2017-05-10 14:45:10 -0700

    Fix issue with image constantly being in use by process

---
+++ picture-review-and-move/Converters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace picture_review_and_move
+{
+    public class UriToCachedImageConverter : IValueConverter
+    {
+        // http://www.thejoyofcode.com/WPF_Image_element_locks_my_local_file.aspx
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!string.IsNullOrEmpty(value?.ToString()))
+            {
+                var bi = new BitmapImage();
+                bi.BeginInit();
+                bi.UriSource = new Uri(value.ToString());
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.EndInit();
+                return bi;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException("Two way conversion is not supported.");
+        }
+    }
+}
