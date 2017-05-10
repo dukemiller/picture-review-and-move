e38fc97d16f64c6c36ee7fa37eb6c7cf458e215c
dukemiller <dukemiller928@hotmail.com>
2017-05-10 14:21:53 -0700

    Add viewmodel and relaycommand

---
+++ picture-review-and-move/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using picture_review_and_move.Annotations;
+
+namespace picture_review_and_move
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ picture-review-and-move/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace picture_review_and_move
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _action;
+
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action action) : this(action, () => true) { }
+
+        public RelayCommand(Action action, Func<bool> canExecute)
+        {
+            this._action = action;
+            this._canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute();
+
+        public void Execute(object parameter) => _action();
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var canExecuteChanged = CanExecuteChanged;
+            canExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
